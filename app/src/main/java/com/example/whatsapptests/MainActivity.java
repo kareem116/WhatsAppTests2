223a108ff24841b8265ddc9650db8ff0626e1c9e
EGYPT_LAPTOP <karimalla@gmail.com>
2021-01-19 15:52:52 +0200

    test

---
+++ app/src/main/java/com/example/whatsapptests/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.whatsapptests;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
